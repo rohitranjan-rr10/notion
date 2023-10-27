c40c27ba9afa4fc8e53682158c5fb8885f504ac8
Rohit Ranjan <rohitranjan.rr10@gmail.com>
2023-10-28 00:26:38 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,58 @@
+import { Toaster } from "sonner";
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { EdgeStoreProvider } from "@/lib/edgestore";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Your connected workspace for wiki, docs & projects | Notion",
+  description:
+    "A new tool that blends your everyday work apps into one. It's the all-in-one workspace for you and your team.",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/logo.svg",
+        href: "/logo.svg",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/logo.svg",
+        href: "/logo.svg",
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ConvexClientProvider>
+          <EdgeStoreProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+              storageKey="notion-theme-key"
+            >
+              <Toaster position="bottom-center" />
+              <ModalProvider />
+              {children}
+            </ThemeProvider>
+          </EdgeStoreProvider>
+        </ConvexClientProvider>
+      </body>
+    </html>
+  );
+}
---
+++ convex/auth.config.js
@@ -0,0 +1,10 @@
+const myObject = {
+  providers: [
+    {
+      domain: "https://bright-falcon-61.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default myObject;
